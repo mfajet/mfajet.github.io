030f050a5df98a29db31f1578415953df0befd53
Mark Fajet <markfajet@gmail.com>
2017-02-18 15:40:24 -0500

    Background changes

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ var loadMore = document.createElement("button");
 loadMore.appendChild(document.createTextNode("MORE FACES!!!!"));
 var i = 1;
 var addMoreImages = function(){
+    var bgNum = Math.floor(Math.random()*61 + 1);
+    if (bgNum == 13 || bgNum ==31){
+        bgNum++;
+    }
+    var body =document.body;
+    body.background= "url(images/face" + bgNum + ".PNG)";
+
     for(var j=0; j<=5; j++){
         if((i+j)%62!=13 && (i+j)%62!=31 && (i+j)%62!=0){
             var face = document.createElement("img");
