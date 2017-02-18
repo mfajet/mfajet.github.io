ab161050d40e26c613b20f024168b694f340f98f
Mark Fajet <markfajet@gmail.com>
2017-02-18 02:51:33 -0500

    bug fix

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var addMoreImages = function(){
     for(var j=0; j<=5; j++){
         if((i+j)%62!=13 && (i+j)%62!=31 && (i+j)%62!=0){
             var face = document.createElement("img");
-            face.src = "/images/face" + (i+j)%62 + ".PNG";
+            face.src = "/markFaces/face" + (i+j)%62 + ".PNG";
             face.setAttribute("width", "90%");
             mainDiv.appendChild(face);
         }else{
