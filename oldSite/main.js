546200703d1ec8087665f0c9b15dc8df0d1f6486
mfajet <mfajet@fiu.edu>
2018-01-23 13:42:28 -0500

    Compressed images

--- oldSite/main.js
+++ oldSite/main.js
@@ -9,12 +9,12 @@ var addMoreImages = function(){
         bgNum++;
     }
     var body =document.body;
-    body.style.background= "url(images/face" + bgNum + ".PNG)";
+    body.style.background= "url(images/face" + bgNum + ".PNG.webp)";
 
     for(var j=0; j<=5; j++){
         if((i+j)%62!=13 && (i+j)%62!=31 && (i+j)%62!=0){
             var face = document.createElement("img");
-            face.src = "images/face" + (i+j)%62 + ".PNG";
+            face.src = "images/face" + (i+j)%62 + ".PNG.webp";
             face.setAttribute("width", "90%");
             mainDiv.appendChild(face);
         }else{
