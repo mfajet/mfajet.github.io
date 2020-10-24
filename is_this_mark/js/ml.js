e3c3dd8bcc5bc9bfdef2b886215da589f8c9264a
Mark Fajet <markfajet@gmail.com>
2020-10-24 17:42:23 -0400

    Add ML game

---
+++ is_this_mark/js/ml.js
@@ -0,0 +1,38 @@
+// import * as tf from '@tensorflow/tfjs';
+// import {loadGraphModel} from '@tensorflow/tfjs-converter';
+
+const MODEL_URL = './js_model/model.json';
+
+const get_model = tf.loadLayersModel(MODEL_URL);
+MODEL = null;
+get_model.then((model)=> {
+  MODEL= model;
+});
+
+
+const isItMark = () =>{
+  const header = document.getElementById('question');
+  const face = document.getElementById('face');
+  faceArr = tf.browser.fromPixels(face).resizeBilinear([250,250]);
+
+  prediction = MODEL.predict(tf.expandDims(faceArr, 0));
+  console.log(prediction);
+  value = prediction.dataSync()[0];
+  console.log(value);
+  const isMark = (value < 0.5);
+  if (isMark){
+    header.innerHTML += ' Yes!';
+  }else{
+    header.innerHTML += ' No!';
+  }
+}
+
+const newPhoto = () => {
+    const header = document.getElementById('question');
+    const face = document.getElementById('face');
+    header.innerHTML = 'Is this Mark?'
+    const currentSrcNum = parseInt(face.src.substr(-5))
+    console.log(face.src);
+    console.log(face.src.substr("images/face".length));
+    face.src = "images/face" + ((currentSrcNum % 12) +1)  +".JPG";
+}
