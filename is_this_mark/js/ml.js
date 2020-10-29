10c8a138aa3e1380e9dad9c1ff846f5bcea3e652
Mark Fajet <markfajet@gmail.com>
2020-10-29 19:23:27 -0400

    Some cleanup

--- is_this_mark/js/ml.js
+++ is_this_mark/js/ml.js
@@ -6,29 +6,32 @@ if (/Mobi/.test(navigator.userAgent)) {
 }
 
 const onProgress = (progress) => {
-  console.log(progress);
   const progressBar = document.getElementById('progress-bar');
   progressBar.innerHTML = (progress * 100) + "%";
   progressBar.setAttribute("style", "width:" + (progress * 100) + "%;");
 }
 const get_model = tf.loadLayersModel(MODEL_URL, {onProgress});
 
-MODEL = null;
+let MODEL = null;
 get_model.then((model)=> {
-  MODEL= model;
+  MODEL = model;
+  const warmupResult = MODEL.predict(tf.zeros([1,250,250,3]));
+  warmupResult.dataSync();
+  warmupResult.dispose();
+
 });
 
 
 const isItMark = async () =>{
   const header = document.getElementById('question');
   const face = document.getElementById('face');
-  faceArr = tf.browser.fromPixels(face).resizeBilinear([250,250]);
+  const faceArr = tf.browser.fromPixels(face).resizeBilinear([250,250]);
   if(!MODEL){
     alert("The model has not finished loading. Check the progress bar");
     return;
   }
-  prediction = MODEL.predict(tf.expandDims(faceArr, 0));
-  value = prediction.dataSync()[0];
+  const prediction = MODEL.predict(tf.expandDims(faceArr, 0));
+  const value = prediction.dataSync()[0];
   prediction.dispose();
   const isMark = (value < 0.5);
   if (isMark){
